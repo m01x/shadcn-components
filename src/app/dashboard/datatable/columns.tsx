a87c10f5fcf0c92fe42eaf5153699ef3d4421caf
m01x <fortiz.informatica@gmail.com>
2025-06-18 14:56:36 -0400

    feat ( datatable ): Column , data-table , dropdown-menu. Todo: tarea de implementacion.

--- src/app/dashboard/datatable/columns.tsx
+++ src/app/dashboard/datatable/columns.tsx
@@ -2,7 +2,20 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Payment } from "@/data/payments.data"
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef, SortDirection } from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon, ChevronUpIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { toast } from "sonner";
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -28,6 +41,20 @@ const dataStatus: Record<StatusKey, StatusColor> = {
 };
 
 
+const SortedIcon = ({ isSorted }: { isSorted: false | SortDirection }) => {
+
+  if ( isSorted === "asc" ) {
+    return <ChevronUpIcon className="h-4 w-4" />;
+  }
+
+  if ( isSorted === "desc") {
+    return <ChevronDownIcon className="h-4 w-4" />;
+  }
+
+  return null;
+  
+}
+
 
 export const columns: ColumnDef<Payment>[] = [
     {
@@ -41,7 +68,17 @@ export const columns: ColumnDef<Payment>[] = [
     },
     {
         accessorKey: "email",
-        header: "Email",
+        header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Email
+            <SortedIcon isSorted={column.getIsSorted()} />
+          </Button>
+        )
+    },
     },
     {
         accessorKey: "amount",
@@ -60,4 +97,37 @@ export const columns: ColumnDef<Payment>[] = [
         accessorKey: "clientName",
         header: "Client name",
     },
+     {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id);
+                toast("Payment ID copied to clipboard", {
+                  position: "top-right",
+                })
+            }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
 ]
--- src/app/dashboard/datatable/data-table.tsx
+++ src/app/dashboard/datatable/data-table.tsx
@@ -1,10 +1,14 @@
 "use client"
+import { useState } from "react";
 
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
+  SortingState,
+  getSortedRowModel,
 } from "@tanstack/react-table"
 
 import {
@@ -15,6 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -25,10 +30,20 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+
+
+  const [sorting, setSorting] = useState<SortingState>([])
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   })
 
   return (
@@ -75,6 +90,24 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+      <div className="flex items-center justify-end space-x-2 py-4 mx-2">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   )
 }
